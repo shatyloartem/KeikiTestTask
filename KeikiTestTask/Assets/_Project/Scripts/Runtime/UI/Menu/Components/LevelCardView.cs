53107bf9d40abf2401e83923a72cf574d6608e00
shatyloartem <shatyloartem@gmail.com>
2026-07-26 22:22:00 +0300

    feat (cardView): implemented clickable button according to design

--- KeikiTestTask/Assets/_Project/Scripts/Runtime/UI/Menu/Components/LevelCardView.cs
+++ KeikiTestTask/Assets/_Project/Scripts/Runtime/UI/Menu/Components/LevelCardView.cs
@@ -1,26 +1,68 @@
 using System;
 using Runtime.Domain.Levels;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace Runtime.UI.Menu.Components
 {
-    public sealed class LevelCardView : MonoBehaviour
+    public sealed class LevelCardView : MonoBehaviour,
+        IPointerDownHandler,
+        IPointerUpHandler,
+        IPointerExitHandler
     {
         [SerializeField] private Button _button;
         [SerializeField] private Image _icon;
+        [SerializeField] private RectTransform _pressTarget;
+        [SerializeField, Min(0f)] private float _pressDepth = 24f;
+        [SerializeField, Min(0f)] private float _pressSpeed = 600f;
 
         private LevelDefinition _level;
         private Action<LevelDefinition> _clickHandler;
+        private Vector2 _releasedPosition;
+        private Vector2 _targetPosition;
 
         private void Awake()
         {
+            _releasedPosition = _pressTarget.anchoredPosition;
+            _targetPosition = _releasedPosition;
             _button.onClick.AddListener(HandleClick);
+        }
+
+        private void Update()
+        {
+            _pressTarget.anchoredPosition = Vector2.MoveTowards(
+                _pressTarget.anchoredPosition,
+                _targetPosition,
+                _pressSpeed * Time.unscaledDeltaTime);
+        }
+
+        private void OnDisable()
+        {
+            Release(immediate: true);
         }
 
         private void OnDestroy()
         {
             _button.onClick.RemoveListener(HandleClick);
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            if (!_button.IsInteractable())
+                return;
+
+            _targetPosition = _releasedPosition + Vector2.down * _pressDepth;
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            Release();
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            Release();
         }
 
         public void Bind(
@@ -52,6 +94,14 @@ namespace Runtime.UI.Menu.Components
         {
             if (_level != null)
                 _clickHandler?.Invoke(_level);
+        }
+
+        private void Release(bool immediate = false)
+        {
+            _targetPosition = _releasedPosition;
+
+            if (immediate)
+                _pressTarget.anchoredPosition = _releasedPosition;
         }
     }
 }
